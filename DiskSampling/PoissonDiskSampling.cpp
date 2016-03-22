723dccede7246e9981f1a16ad0d9e1aad7b74dea
Chris Ohk <utilForever@gmail.com>
2016-03-22 20:53:25 +0900

    Implement GeneratePointAround(), IsInRectangle(), IsInNeighbourhood()

--- DiskSampling/PoissonDiskSampling.cpp
+++ DiskSampling/PoissonDiskSampling.cpp
@@ -7,7 +7,7 @@ PoissonDiskSampling::PoissonDiskSampling(int pointWidth, int pointHeight, double
 	m_height(pointHeight),
 	m_minDist(pointMinDist),
 	m_pointCount(pointCount),
-	m_cellSize(m_minDist / 1.414214f),
+	m_cellSize(m_minDist / 1.414214),
 	m_gridWidth(ceil(m_width / m_cellSize)),
 	m_gridHeight(ceil(m_height / m_cellSize)),
 	m_grid(std::vector<std::vector<std::shared_ptr<Point>>>(m_gridWidth, std::vector<std::shared_ptr<Point>>(m_gridHeight, nullptr)))
@@ -50,4 +50,37 @@ std::vector<std::pair<double, double>> PoissonDiskSampling::Generate()
 	}
 
 	return m_sample;
+}
+
+PoissonDiskSampling::Point PoissonDiskSampling::GeneratePointAround(Point p)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	double r1 = static_cast<double>(gen()) / gen.max();
+	double r2 = static_cast<double>(gen()) / gen.max();
+
+	double radius = m_minDist * (r1 + 1);
+
+	double angle = 2 * 3.14159265 * r2;
+
+	double newX = p.x + radius * cos(angle);
+	double newY = p.y + radius * sin(angle);
+
+	return Point(newX, newY);
+}
+
+bool PoissonDiskSampling::IsInRectangle(Point p)
+{
+	return (p.x >= 0 && p.y >= 0 && p.x < m_width && p.y < m_height);
+}
+
+bool PoissonDiskSampling::IsInNeighbourhood(Point p)
+{
+	std::vector<std::shared_ptr<Point>> cells = GetCellsAround(p);
+	int size = cells.size();
+	for (int i = 0; i < size; ++i)
+	{
+		
+	}
 }
