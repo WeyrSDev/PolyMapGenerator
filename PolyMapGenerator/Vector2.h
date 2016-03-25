2aca856851480580ac954e47b3f3c2913beacdef
Chris Ohk <utilForever@gmail.com>
2016-03-25 19:02:32 +0900

    Declare Vector2's special function

---
+++ PolyMapGenerator/Vector2.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(double angle);
+	Vector2(double _x, double _y);
+	Vector2(const Vector2& v1, const Vector2& v2);
+	
+	~Vector2();
+
+	Vector2(const Vector2& v);
+	Vector2(Vector2&& v);
+
+	Vector2& operator=(const Vector2& v);
+	Vector2& operator=(Vector2&& v);
+}
+
+#endif
