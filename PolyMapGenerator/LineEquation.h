647bf5b2a8bcd4b872706866df24a907f7697330
Chris Ohk <utilForever@gmail.com>
2016-04-03 16:32:32 +0900

    Declare LineEquation class

--- PolyMapGenerator/LineEquation.h
+++ PolyMapGenerator/LineEquation.h
@@ -1,4 +1,33 @@
 #ifndef LINE_EQUATION_H
 #define LINE_EQUATION_H
 
+#include "Vector2.h"
+
+class LineEquation
+{
+public:
+	LineEquation();
+	LineEquation(Vector2 a, Vector2 b);
+	LineEquation(Vector2 p, double m);
+	
+	~LineEquation();
+
+	LineEquation(const LineEquation& e);
+	LineEquation(LineEquation&& e);
+
+	LineEquation& operator=(const LineEquation& e);
+	LineEquation& operator=(LineEquation&& e);
+
+	double operator()(const double x);
+
+	void Move(const Vector2 v);
+	Vector2 Intersection(LineEquation& e) const;
+	bool IsHorizontal();
+	bool IsVertical();
+
+	double m;
+	double b;
+	bool vertical;
+};
+
 #endif
