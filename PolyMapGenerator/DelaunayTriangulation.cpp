cc41eb9f1e11a57f711e54347389762bea12ba4b
Chris Ohk <utilForever@gmail.com>
2016-04-11 06:26:17 +0900

    Implement Triangle::SetCircumstanceCircle()

--- PolyMapGenerator/DelaunayTriangulation.cpp
+++ PolyMapGenerator/DelaunayTriangulation.cpp
@@ -14,6 +14,48 @@ namespace DelaunayTriangulation
 
 		double x2 = m_vertices[2]->GetX();
 		double y2 = m_vertices[2]->GetY();
-	}
 
+		double y10 = y1 - y0;
+		double y21 = y2 - y1;
+
+		bool b21zero = y21 > -EPSILON && y21 < EPSILON;
+
+		if (y10 > -EPSILON && y10 < EPSILON)
+		{
+			if (b21zero)
+			{
+				if (x1 > x0)
+				{
+					if (x2 > x1)
+					{
+						x1 = x2;
+					}
+				}
+				else
+				{
+					if (x2 < x0)
+					{
+						x0 = x2;
+					}
+				}
+
+				m_center.x = (x0 + x1) * 0.5;
+				m_center.y = y0;
+			}
+			else
+			{
+				double m1 = -(x2 - x1) / y21;
+
+				double mx1 = (x1 + x2) * 0.5;
+				double my1 = (x1 + x2) * 0.5;
+
+				m_center.x = (x0 + x1) * 0.5;
+				m_center.y = m1 * (m_center.x - mx1) + my1;
+			}
+		}
+		else if (b21zero)
+		{
+			
+		}
+	}
 }
