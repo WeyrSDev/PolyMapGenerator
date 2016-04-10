9970784bc9d5ef629a233e1e76884491b7491fa7
Chris Ohk <utilForever@gmail.com>
2016-04-10 23:21:24 +0900

    Get x, y of points

--- PolyMapGenerator/DelaunayTriangulation.cpp
+++ PolyMapGenerator/DelaunayTriangulation.cpp
@@ -8,6 +8,12 @@ namespace DelaunayTriangulation
 	{
 		double x0 = m_vertices[0]->GetX();
 		double y0 = m_vertices[0]->GetY();
+
+		double x1 = m_vertices[1]->GetX();
+		double y1 = m_vertices[1]->GetY();
+
+		double x2 = m_vertices[2]->GetX();
+		double y2 = m_vertices[2]->GetY();
 	}
 
 }
