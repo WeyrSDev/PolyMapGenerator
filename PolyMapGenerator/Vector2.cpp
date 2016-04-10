cf00d10d1b85e523cc60b18e9c6e529b11e43398
Chris Ohk <utilForever@gmail.com>
2016-04-10 23:34:43 +0900

    Modify macro to const variable

--- PolyMapGenerator/Vector2.cpp
+++ PolyMapGenerator/Vector2.cpp
@@ -2,13 +2,8 @@
 
 #include <ostream>
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846264338327
-#endif
-
-#ifndef EQ_THRESHOLD
-#define EQ_THRESHOLD 0.00001
-#endif
+const double M_PI = 3.14159265358979323846264338327;
+const double EQ_THRESHOLD = 0.00001;
 
 std::ostream& operator<<(std::ostream& os, const Vector2& v)
 {
