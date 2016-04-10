0460eaca97afb78af3107c1458c0377f94aea87f
Chris Ohk <utilForever@gmail.com>
2016-04-10 23:18:31 +0900

    Add DelaunayTriangulation.cpp

---
+++ PolyMapGenerator/DelaunayTriangulation.cpp
@@ -0,0 +1,13 @@
+#include "DelaunayTriangulation.h"
+
+namespace DelaunayTriangulation
+{
+	const double sqtr3 = 1.732050808;
+
+	void Triangle::SetCircumstanceCircle()
+	{
+		double x0 = m_vertices[0]->GetX();
+		double y0 = m_vertices[0]->GetY();
+	}
+
+}
