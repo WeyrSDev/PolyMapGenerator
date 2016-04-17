da4bfcc32aa68a87a93d10d526335ead687f3e4f
Chris Ohk <utilForever@gmail.com>
2016-04-18 01:15:23 +0900

    Declare preprocessors and QuadTree class

---
+++ PolyMapGenerator/QuadTree.h
@@ -0,0 +1,21 @@
+#ifndef QUADTREE_H
+#define QUADTREE_H
+
+#include <vector>
+#include <SFML/System.hpp>
+
+#include "Math/Vector2.h"
+
+// Forward Declaration
+struct AABB;
+
+template <typename T>
+class QuadTree
+{
+public:
+
+private:
+
+};
+
+#endif
