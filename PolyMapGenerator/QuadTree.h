7c23ec01028799afcb7493a534a141efb7740073
Chris Ohk <utilForever@gmail.com>
2016-04-18 10:45:48 +0900

    Declare member variables of QuadTree class

--- PolyMapGenerator/QuadTree.h
+++ PolyMapGenerator/QuadTree.h
@@ -15,7 +15,17 @@ class QuadTree
 public:
 
 private:
+	std::vector<T> m_elements;
+	std::vector<AABB> m_elementRegions;
 
+	QuadTree* m_northWest;
+	QuadTree* m_northEast;
+	QuadTree* m_southEast;
+	QuadTree* m_southWest;
+
+	bool m_divided;
+	int m_branchDepth;
+	int m_elementsBranch;
 };
 
 #endif
