9ce7c15500fc2f80ff4ed6f87e3dae7491e281c0
Chris Ohk <utilForever@gmail.com>
2016-04-18 11:11:19 +0900

    Declare special functions of QuadTree class

--- PolyMapGenerator/QuadTree.h
+++ PolyMapGenerator/QuadTree.h
@@ -13,6 +13,31 @@ template <typename T>
 class QuadTree
 {
 public:
+	QuadTree() :
+		m_northWest(nullptr), m_northEast(nullptr),
+		m_southEast(nullptr), m_southWest(nullptr),
+		m_divided(false), m_branchDepth(0), m_elementsBranch(0) { }
+	QuadTree(AABB boundary, int depth) :
+		m_boundary(boundary), m_divided(false), m_branchDepth(depth), m_elementsBranch(0) { }
+
+	~QuadTree()
+	{
+		if (m_divided)
+		{
+			delete m_northWest;
+			delete m_northEast;
+			delete m_southEast;
+			delete m_southWest;
+		}
+	}
+
+	QuadTree(const QuadTree& qt) = delete;
+	QuadTree(QuadTree&& qt) = delete;
+
+	QuadTree& operator=(const QuadTree& qt) = delete;
+	QuadTree& operator=(QuadTree&& qt) = delete;
+
+	AABB m_boundary;
 
 private:
 	std::vector<T> m_elements;
