bcc6e1e952df31d96efb7b0e0e1198e103d16b2b
Chris Ohk <utilForever@gmail.com>
2016-04-24 22:30:15 +0900

    Declare struct Center's member variables

--- PolyMapGenerator/Structure.h
+++ PolyMapGenerator/Structure.h
@@ -31,4 +31,24 @@ enum class BiomeType
 struct Edge;
 struct Corner;
 
+struct Center
+{
+	unsigned int m_index;
+	Vector2 m_positon;
+
+	bool m_water;
+	bool m_ocean;
+	bool m_coast;
+	bool m_border;
+	BiomeType m_biome;
+	double m_elevation;
+	double m_moisture;
+
+	std::vector<Edge*> m_edges;
+	std::vector<Corner*> m_corners;
+	std::vector<Center*> m_centers;
+
+	using CenterIterator = std::vector<Center*>::iterator;
+};
+
 #endif
