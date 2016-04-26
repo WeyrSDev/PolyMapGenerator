056aa108011c6f8d97968ef51e12c2e75e444694
Chris Ohk <utilForever@gmail.com>
2016-04-26 13:02:28 +0900

    Implement struct Center's methods

--- PolyMapGenerator/Structure.cpp
+++ PolyMapGenerator/Structure.cpp
@@ -6,3 +6,58 @@ Edge::Edge(unsigned int index, Center* center1, Center* center2, Corner* corner1
 {
 	
 }
+
+bool Center::RemoveEdge(Edge* e)
+{
+	for (auto edgeIter = m_edges.begin(); edgeIter != m_edges.end(); ++edgeIter)
+	{
+		if (*edgeIter == e)
+		{
+			m_edges.erase(edgeIter);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Center::RemoveCorner(Corner* c)
+{
+	for (auto iter = m_corners.begin(); iter != m_corners.end(); ++iter)
+	{
+		if (*iter == c)
+		{
+			m_corners.erase(iter);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+Edge* Center::GetEdgeWith(Center* c)
+{
+	for (auto iter = m_edges.begin(); iter != m_edges.end(); ++iter)
+	{
+		if ((*iter)->m_d0 == c || (*iter)->m_d1 == c)
+		{
+			return *iter;
+		}
+	}
+
+	return nullptr;
+}
+
+void Center::MakeBorder()
+{
+	m_border = true;
+	m_water = true;
+	m_ocean = true;
+
+	for (auto iter = m_corners.begin(); iter != m_corners.end(); ++iter)
+	{
+		(*iter)->m_border = true;
+		(*iter)->m_water = true;
+		(*iter)->m_ocean = true;
+	}
+}
