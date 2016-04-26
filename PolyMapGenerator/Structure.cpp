77b47ce52e6b52504b81671d79620ef1c836a25b
Chris Ohk <utilForever@gmail.com>
2016-04-26 12:41:33 +0900

    Implement struct Edge's constructor

--- PolyMapGenerator/Structure.cpp
+++ PolyMapGenerator/Structure.cpp
@@ -1,2 +1,8 @@
 #include "Math/LineEquation.h"
 #include "Structure.h"
+
+Edge::Edge(unsigned int index, Center* center1, Center* center2, Corner* corner1, Corner* corner2) :
+	m_index(index), m_d0(center1), m_d1(center2), m_v0(corner1), m_v1(corner2), m_riverVolume(0.0)
+{
+	
+}
