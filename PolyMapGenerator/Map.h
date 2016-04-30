553b037d42a5554eb7518c66f8b08a30f8260e6c
Chris Ohk <utilForever@gmail.com>
2016-04-30 13:06:50 +0900

    Declare class Map's member variables

--- PolyMapGenerator/Map.h
+++ PolyMapGenerator/Map.h
@@ -8,8 +8,6 @@
 #include "Structure.h"
 #include "QuadTree.h"
 
-using pCenterQT = QuadTree<Center*>;
-
 // Forward Declaration
 class Vector2;
 namespace Noise
@@ -33,6 +31,24 @@ public:
 
 	Map& operator=(const Map& map) = delete;
 	Map& operator=(Map&& map) = delete;
+
+private:
+	int m_mapWidth;
+	int m_mapHeight;
+	double m_pointSpread;
+	double m_zCoord;
+	Noise::Module::Perlin* m_noiseMap;
+	std::string m_seed;
+	QuadTree<Center*> m_centersQuadTree;
+
+	std::vector<DelaunayTriangulation::Vertex> m_points;
+
+	std::map<double, std::map<double, Center*>> m_posCenterMap;
+	std::vector<Edge*> m_edges;
+	std::vector<Corner*> m_corners;
+	std::vector<Center*> m_centers;
+
+	static const std::vector<std::vector<BiomeType>> m_elevationMoistureMatrix;
 };
 
 #endif
