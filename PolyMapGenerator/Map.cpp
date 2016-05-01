2149049bca5fc5b28b8a258f62a5091dbb4c7bca
Chris Ohk <utilForever@gmail.com>
2016-05-01 20:25:42 +0900

    Modify header file location

--- PolyMapGenerator/Map.cpp
+++ PolyMapGenerator/Map.cpp
@@ -3,6 +3,6 @@
 #include <SFML/System.hpp>
 
 #include "Map.h"
+#include "PoissonDiskSampling/PoissonDiskSampling.h"
 #include "Math/Vector2.h"
-#include "PoissonDiskSampling.h"
 #include "Noise/Noise.h"
