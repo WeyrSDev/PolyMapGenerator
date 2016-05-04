7d5111d564ff6780078803c3b8841317a906c534
Chris Ohk <utilForever@gmail.com>
2016-05-05 01:51:58 +0900

    Add constants, struct InfoShown

--- PolyMapGeneratorTest/MapTest.cpp
+++ PolyMapGeneratorTest/MapTest.cpp
@@ -5,3 +5,21 @@
 #include "Map.h"
 #include "Structure.h"
 #include "Noise/noise.h"
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+const int POINT_SIZE = 2;
+const int LINE_SIZE = 1;
+
+struct InfoShown
+{
+	enum class Name
+	{
+		Elevation,
+		Moisture,
+		Biomes
+	};
+};
+
+InfoShown::Name VideoMode;
