81086955403fe458c212132442dd11b3d0a3bae1
Chris Ohk <utilForever@gmail.com>
2016-05-07 16:18:02 +0900

    Delete convex hull part

--- PolyMapGeneratorTest/MapTest.cpp
+++ PolyMapGeneratorTest/MapTest.cpp
@@ -83,39 +83,6 @@ void DrawEdge(Edge* e, sf::RenderWindow* window);
 void DrawCorner(Corner* c, sf::RenderWindow* window);
 void DrawCenter(Center* c, sf::RenderWindow* window);
 
-double cross(const Vector2& O, const Vector2& A, const Vector2& B)
-{
-	return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
-}
-
-void convex_hull(std::vector<Corner*>& P)
-{
-	int n = P.size(), k = 0;
-	std::vector<Corner*> H(2 * n);
-
-	// Sort points lexicographically
-	sort(P.begin(), P.end(), [](Corner* c1, Corner* c2)
-	{
-		return c1->m_position.x < c2->m_position.x || (c1->m_position.x == c2->m_position.x && c1->m_position.y < c2->m_position.y);
-	});
-
-	// Build lower hull
-	for (int i = 0; i < n; ++i) {
-		while (k >= 2 && cross(H[k - 2]->m_position, H[k - 1]->m_position, P[i]->m_position) <= 0) k--;
-		H[k++] = P[i];
-	}
-
-	// Build upper hull
-	for (int i = n - 2, t = k + 1; i >= 0; i--) {
-		while (k >= t && cross(H[k - 2]->m_position, H[k - 1]->m_position, P[i]->m_position) <= 0) k--;
-		H[k++] = P[i];
-	}
-
-	H.resize(k - 1);
-
-	P = H;
-}
-
 int main()
 {
 	sf::Clock timer;
